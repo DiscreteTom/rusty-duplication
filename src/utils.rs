9e2df05d4de18c2b17e73ab5043fcaca522a4535
DiscreteTom <discrete_tom@outlook.com>
2023-05-13 17:56:10 +0800

    tests: add unit tests for utils

--- src/utils.rs
+++ src/utils.rs
@@ -32,3 +32,30 @@ impl FrameInfoExt for DXGI_OUTDUPL_FRAME_INFO {
     self.LastPresentTime > 0
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use windows::Win32::Graphics::Dxgi::{DXGI_OUTDUPL_FRAME_INFO, DXGI_OUTPUT_DESC};
+
+  use crate::utils::{FrameInfoExt, OutputDescExt};
+
+  #[test]
+  fn output_desc_ext() {
+    let mut desc = DXGI_OUTPUT_DESC::default();
+    desc.DesktopCoordinates.left = 0;
+    desc.DesktopCoordinates.top = 0;
+    desc.DesktopCoordinates.right = 1920;
+    desc.DesktopCoordinates.bottom = 1080;
+    assert_eq!(desc.width(), 1920);
+    assert_eq!(desc.height(), 1080);
+    assert_eq!(desc.calc_buffer_size(), 1920 * 1080 * 4);
+  }
+
+  #[test]
+  fn frame_info_ext() {
+    let mut desc = DXGI_OUTDUPL_FRAME_INFO::default();
+    assert!(!desc.is_new_frame());
+    desc.LastPresentTime = 1;
+    assert!(desc.is_new_frame());
+  }
+}
