199be30616c01601ae02dd0e42568925352c13ba
DiscreteTom <discrete_tom@outlook.com>
2023-05-13 10:50:54 +0800

    chore: reduce unsafe area

--- src/manager.rs
+++ src/manager.rs
@@ -33,39 +33,39 @@ impl Manager {
   pub fn refresh(&mut self) -> Result<(), &'static str> {
     self.contexts.clear();
 
-    unsafe {
-      let factory =
-        CreateDXGIFactory1::<IDXGIFactory1>().map_err(|_| "CreateDXGIFactory1 failed")?;
-      let mut adapter_outputs = Vec::new();
+    let factory =
+      unsafe { CreateDXGIFactory1::<IDXGIFactory1>() }.map_err(|_| "CreateDXGIFactory1 failed")?;
+    let mut adapter_outputs = Vec::new();
 
-      // collect adapters and outputs
-      for adapter_index in 0.. {
-        let adapter = match factory.EnumAdapters1(adapter_index) {
-          Ok(adapter) => adapter,
+    // collect adapters and outputs
+    for adapter_index in 0.. {
+      let adapter = match unsafe { factory.EnumAdapters1(adapter_index) } {
+        Ok(adapter) => adapter,
+        Err(_) => break,
+      };
+      let mut outputs = Vec::new();
+      for output_index in 0.. {
+        match unsafe { adapter.EnumOutputs(output_index) } {
           Err(_) => break,
-        };
-        let mut outputs = Vec::new();
-        for output_index in 0.. {
-          match adapter.EnumOutputs(output_index) {
-            Err(_) => break,
-            Ok(output) => outputs.push(output),
-          }
-        }
-        if outputs.len() > 0 {
-          adapter_outputs.push((adapter, outputs))
+          Ok(output) => outputs.push(output),
         }
       }
-      if adapter_outputs.len() == 0 {
-        return Err("No output");
+      if outputs.len() > 0 {
+        adapter_outputs.push((adapter, outputs))
       }
+    }
+    if adapter_outputs.len() == 0 {
+      return Err("No output");
+    }
 
-      // prepare device and output
-      for (adapter, outputs) in adapter_outputs {
-        let mut device: Option<ID3D11Device> = None.clone();
-        let mut device_context: Option<ID3D11DeviceContext> = None.clone();
-        let mut feature_level = D3D_FEATURE_LEVEL_9_1;
+    // prepare device and output
+    for (adapter, outputs) in adapter_outputs {
+      let mut device: Option<ID3D11Device> = None.clone();
+      let mut device_context: Option<ID3D11DeviceContext> = None.clone();
+      let mut feature_level = D3D_FEATURE_LEVEL_9_1;
 
-        // create device for each adapter
+      // create device for each adapter
+      unsafe {
         D3D11CreateDevice(
           &adapter,
           D3D_DRIVER_TYPE_UNKNOWN,
@@ -77,26 +77,28 @@ impl Manager {
           Some(&mut feature_level),
           Some(&mut device_context),
         )
-        .map_err(|_| "D3D11CreateDevice failed")?;
-        let device = device.unwrap();
-        let device_context = device_context.unwrap();
-
-        // create duplication output for each output
-        for output in outputs {
-          let output = output.cast::<IDXGIOutput1>().unwrap();
-          let output_duplication = output
-            .DuplicateOutput(&device)
-            .map_err(|_| "DuplicateOutput failed")?;
-          self.contexts.push(DuplicateContext::new(
-            device.clone(),
-            device_context.clone(),
-            output,
-            output_duplication,
-            self.timeout_ms,
-          ))
-        }
       }
-      Ok(())
+      .map_err(|_| "D3D11CreateDevice failed")?;
+      let device = device.unwrap();
+      let device_context = device_context.unwrap();
+
+      // create duplication output for each output
+      for output in outputs {
+        let output = output.cast::<IDXGIOutput1>().unwrap();
+        let output_duplication =
+          unsafe { output.DuplicateOutput(&device) }.map_err(|_| "DuplicateOutput failed")?;
+        self.contexts.push(DuplicateContext::new(
+          device.clone(),
+          device_context.clone(),
+          output,
+          output_duplication,
+          self.timeout_ms,
+        ))
+      }
     }
+    Ok(())
+  }
+}
+
   }
 }
