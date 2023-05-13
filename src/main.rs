94143f5942c381b4118a7f45518ec9ee38c92e58
DiscreteTom <discrete_tom@outlook.com>
2023-05-13 12:24:18 +0800

    chore: update main as an example

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,47 @@ mod duplicate_context;
 mod manager;
 mod utils;
 
-use std::{fs::File, io::Write, thread, time::Duration};
-
+use capturer::model::Capturer;
 use manager::Manager;
+use std::{fs::File, io::Write, thread, time::Duration};
+use utils::OutputDescExt;
 
-use crate::{capturer::model::Capturer, utils::Dimension};
+use crate::utils::FrameInfoExt;
 
-fn main() -> std::io::Result<()> {
+fn main() {
+  // manager will collect monitor info when created
   let manager = Manager::default().unwrap();
-  let mut capturer = manager.contexts[0]
-    .shared_capturer("test".to_string())
-    .unwrap();
-  println!(
-    "size: {}x{}",
-    capturer.get_desc().width(),
-    capturer.get_desc().height()
-  );
+  // you can also refresh monitor info manually
+  // manager.refresh();
 
+  // get monitor info before capturing start
+  for ctx in &manager.contexts {
+    ctx.get_desc().unwrap();
+  }
+
+  // create capturer for a display
+  // this will allocate memory buffer to store pixel data
+  let mut capturer = manager.contexts[0].simple_capturer().unwrap();
+
+  // you can also get monitor info from a capturer
+  let desc = capturer.get_desc().unwrap();
+  // we have some extension methods for you such as `width/height`
+  println!("size: {}x{}", desc.width(), desc.height());
+
+  // sleep for a while before capture to wait system update
   thread::sleep(Duration::from_millis(100));
 
-  capturer.capture().unwrap();
+  // capture desktop image and get the frame info
+  // `safe_capture` will check if the buffer's size is enough
+  let info = capturer.safe_capture().unwrap();
 
-  let mut file = File::create("capture.bin")?;
-  file.write_all(capturer.get_buffer())?;
-  Ok(())
+  // check if this is a new frame using the extension method `is_new_frame`
+  if info.is_new_frame() {
+    println!("captured!");
+  }
+
+  // write to a file
+  let mut file = File::create("capture.bin").unwrap();
+  // `get_buffer` will return `&[u8]` in BGRA32 format
+  file.write_all(capturer.get_buffer()).unwrap();
 }
