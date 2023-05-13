684a0cc3a7629f0ba33540144425198f2b4e2534
DiscreteTom <discrete_tom@outlook.com>
2023-05-13 11:55:56 +0800

    chore: add FrameInfoExt

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use std::result;
 
-use windows::Win32::Graphics::Dxgi::DXGI_OUTPUT_DESC;
+use windows::Win32::Graphics::Dxgi::{DXGI_OUTDUPL_FRAME_INFO, DXGI_OUTPUT_DESC};
 
 pub trait OutputDescExt {
   fn width(&self) -> u32;
@@ -21,4 +21,14 @@ pub type Result<T> = result::Result<T, &'static str>;
 /// Return needed buffer size, in bytes.
 pub fn calc_buffer_size(desc: DXGI_OUTPUT_DESC) -> usize {
   (desc.width() * desc.height() * 4) as usize // 4 for BGRA32
+}
+
+pub trait FrameInfoExt {
+  fn is_new_frame(&self) -> bool;
+}
+
+impl FrameInfoExt for DXGI_OUTDUPL_FRAME_INFO {
+  fn is_new_frame(&self) -> bool {
+    self.LastPresentTime > 0
+  }
 }
