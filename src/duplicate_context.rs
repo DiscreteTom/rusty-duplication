e606d85804707c92d0b2b2d1ce406374bf1bd8a5
DiscreteTom <discrete_tom@outlook.com>
2023-05-13 10:57:16 +0800

    chore: reduce unsafe area

--- src/duplicate_context.rs
+++ src/duplicate_context.rs
@@ -43,72 +43,70 @@ impl DuplicateContext {
   }
 
   pub fn get_desc(&self) -> DXGI_OUTPUT_DESC {
-    unsafe {
-      let mut desc = DXGI_OUTPUT_DESC::default();
-      self.output.GetDesc(&mut desc).unwrap();
-      desc
-    }
+    let mut desc = DXGI_OUTPUT_DESC::default();
+    unsafe { self.output.GetDesc(&mut desc) }.unwrap();
+    desc
   }
 
   pub fn create_readable_texture(&self) -> ID3D11Texture2D {
+    let desc = self.get_desc();
+
+    // create a readable texture description
+    let texture_desc = D3D11_TEXTURE2D_DESC {
+      BindFlags: D3D11_BIND_FLAG::default(),
+      CPUAccessFlags: D3D11_CPU_ACCESS_READ,
+      MiscFlags: D3D11_RESOURCE_MISC_FLAG::default(),
+      Usage: D3D11_USAGE_STAGING, // A resource that supports data transfer (copy) from the GPU to the CPU.
+      Width: desc.width(),
+      Height: desc.height(),
+      MipLevels: 1,
+      ArraySize: 1,
+      Format: DXGI_FORMAT_B8G8R8A8_UNORM,
+      SampleDesc: DXGI_SAMPLE_DESC {
+        Count: 1,
+        Quality: 0,
+      },
+    };
+
+    // create a readable texture in GPU memory
+    let mut readable_texture: Option<ID3D11Texture2D> = None.clone();
     unsafe {
-      let desc = self.get_desc();
-
-      // create a readable texture description
-      let texture_desc = D3D11_TEXTURE2D_DESC {
-        BindFlags: D3D11_BIND_FLAG::default(),
-        CPUAccessFlags: D3D11_CPU_ACCESS_READ,
-        MiscFlags: D3D11_RESOURCE_MISC_FLAG::default(),
-        Usage: D3D11_USAGE_STAGING, // A resource that supports data transfer (copy) from the GPU to the CPU.
-        Width: desc.width(),
-        Height: desc.height(),
-        MipLevels: 1,
-        ArraySize: 1,
-        Format: DXGI_FORMAT_B8G8R8A8_UNORM,
-        SampleDesc: DXGI_SAMPLE_DESC {
-          Count: 1,
-          Quality: 0,
-        },
-      };
-
-      // create a readable texture in GPU memory
-      let mut readable_texture: Option<ID3D11Texture2D> = None.clone();
       self
         .device
         .CreateTexture2D(&texture_desc, None, Some(&mut readable_texture))
-        .unwrap();
-      let readable_texture = readable_texture.unwrap();
-      // Lower priorities causes stuff to be needlessly copied from gpu to ram,
-      // causing huge ram usage on some systems.
-      // https://github.com/bryal/dxgcap-rs/blob/208d93368bc64aed783791242410459c878a10fb/src/lib.rs#L225
-      readable_texture.SetEvictionPriority(DXGI_RESOURCE_PRIORITY_MAXIMUM.0);
-
-      readable_texture
     }
+    .unwrap();
+    let readable_texture = readable_texture.unwrap();
+    // Lower priorities causes stuff to be needlessly copied from gpu to ram,
+    // causing huge ram usage on some systems.
+    // https://github.com/bryal/dxgcap-rs/blob/208d93368bc64aed783791242410459c878a10fb/src/lib.rs#L225
+    unsafe { readable_texture.SetEvictionPriority(DXGI_RESOURCE_PRIORITY_MAXIMUM.0) };
+
+    readable_texture
   }
 
   pub fn acquire_next_frame(
     &self,
     readable_texture: &ID3D11Texture2D,
   ) -> (IDXGISurface1, DXGI_OUTDUPL_FRAME_INFO) {
+    // acquire GPU texture
+    let mut frame_info = DXGI_OUTDUPL_FRAME_INFO::default();
+    let mut resource: Option<IDXGIResource> = None.clone();
     unsafe {
-      // acquire GPU texture
-      let mut frame_info = DXGI_OUTDUPL_FRAME_INFO::default();
-      let mut resource: Option<IDXGIResource> = None.clone();
       self
         .output_duplication
         .AcquireNextFrame(self.timeout_ms, &mut frame_info, &mut resource)
-        .unwrap();
-      let texture: ID3D11Texture2D = resource.unwrap().cast().unwrap();
-
-      // copy GPU texture to readable texture
-      self.device_context.CopyResource(readable_texture, &texture);
-
-      // release GPU texture
-      self.output_duplication.ReleaseFrame().unwrap();
-
-      (readable_texture.cast().unwrap(), frame_info)
     }
+    .unwrap();
+    let texture: ID3D11Texture2D = resource.unwrap().cast().unwrap();
+
+    // copy GPU texture to readable texture
+    unsafe { self.device_context.CopyResource(readable_texture, &texture) };
+
+    // release GPU texture
+    unsafe { self.output_duplication.ReleaseFrame() }.unwrap();
+
+    (readable_texture.cast().unwrap(), frame_info)
   }
 
   pub fn capture_frame(
@@ -117,15 +115,15 @@ impl DuplicateContext {
     len: usize,
     readable_texture: &ID3D11Texture2D,
   ) -> DXGI_OUTDUPL_FRAME_INFO {
-    unsafe {
-      let (frame, info) = self.acquire_next_frame(readable_texture);
-      let mut mapped_surface = DXGI_MAPPED_RECT::default();
+    let (frame, info) = self.acquire_next_frame(readable_texture);
+    let mut mapped_surface = DXGI_MAPPED_RECT::default();
 
+    unsafe {
       frame.Map(&mut mapped_surface, DXGI_MAP_READ).unwrap();
       ptr::copy_nonoverlapping(mapped_surface.pBits, dest, len);
       frame.Unmap().unwrap();
-
-      info
     }
+
+    info
   }
 }
