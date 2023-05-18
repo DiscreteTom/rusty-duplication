61d0385b2e0989943ef43d50e5fc02cc12be61b5
DiscreteTom <discrete_tom@outlook.com>
2023-05-18 08:53:26 +0800

    chore: remove model.PointerShape

--- src/model.rs
+++ src/model.rs
@@ -1,9 +1,3 @@
 use std::result;
-use windows::Win32::Graphics::Dxgi::DXGI_OUTDUPL_POINTER_SHAPE_INFO;
 
 pub type Result<T> = result::Result<T, String>;
-
-pub struct PointerShape<'a> {
-  pub info: DXGI_OUTDUPL_POINTER_SHAPE_INFO,
-  pub data: &'a [u8],
-}
