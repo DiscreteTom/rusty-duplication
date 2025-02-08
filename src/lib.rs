907af83cdc0dfb8962a3e1d44a3536bc287277df
DiscreteTom <discrete_tom@outlook.com>
2025-02-08 20:01:51 +0800

    chore!: rename util mod to ext

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod error;
+mod ext;
 mod monitor;
 mod scanner;
-mod utils;
 
 pub mod capturer;
 pub mod model;
 
 pub use error::*;
+pub use ext::*;
 pub use monitor::*;
 pub use scanner::*;
-pub use utils::*;
