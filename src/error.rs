92594c8e8b8e2d208729f419907c4a02b8f93c79
DiscreteTom <discrete_tom@outlook.com>
2025-02-08 16:22:10 +0800

    chore: optimize code for error, add unit tests

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,13 @@
 use thiserror::Error;
 
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Clone)]
 pub enum Error {
-  #[error("invalid buffer length")]
-  InvalidBufferLength,
   #[error("no output available")]
   NoOutput,
+
+  #[error("invalid buffer length")]
+  InvalidBufferLength,
+
   /// A Windows error.
   #[error("{api}: {err}")]
   Windows {
@@ -15,14 +17,41 @@ pub enum Error {
 }
 
 impl Error {
-  pub(crate) fn windows(api: &'static str, err: windows::core::Error) -> Error {
-    Error::Windows { api, err }
+  /// Create a new Windows error.
+  #[inline]
+  pub(crate) const fn windows(api: &'static str, err: windows::core::Error) -> Self {
+    Self::Windows { api, err }
   }
 
-  pub(crate) fn from_win32(api: &'static str) -> Error {
-    Error::Windows {
-      api,
-      err: windows::core::Error::from_win32(),
-    }
+  /// Create a new Windows error from `GetLastError`.
+  #[inline]
+  pub(crate) fn from_win32(api: &'static str) -> Self {
+    Self::windows(api, windows::core::Error::from_win32())
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::Error;
+
+  #[test]
+  fn format_error() {
+    assert_eq!(format!("{}", Error::NoOutput), "no output available");
+
+    assert_eq!(
+      format!("{}", Error::InvalidBufferLength),
+      "invalid buffer length"
+    );
+
+    assert_eq!(
+      format!(
+        "{}",
+        Error::Windows {
+          api: "api",
+          err: windows::core::Error::empty()
+        }
+      ),
+      "api: The operation completed successfully. (0x00000000)"
+    );
   }
 }
